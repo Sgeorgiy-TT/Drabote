8aef1e49320f83b4cc20b2451238e0360c28559a
Georgiy Savin <goshenka.savin.00@mail.ru>
2026-03-13 02:34:22 +0300

    Initial commit with all files

--- TelegramCasinoBot/Logging/FileLoggerOptions.cs
+++ TelegramCasinoBot/Logging/FileLoggerOptions.cs
@@ -4,8 +4,8 @@ namespace TelegramMetroidvaniaBot.Logging
     {
         public string Path { get; set; } = "Logs/log-.txt";
         public bool Append { get; set; } = true;
-        public long FileSizeLimitBytes { get; set; } = 10485760; // 10 MB
+        public long FileSizeLimitBytes { get; set; } = 10485760; 
         public int MaxRollingFiles { get; set; } = 7;
-        public bool SeparateFilesByCategory { get; set; } = false; // Записывать ли логи в разные файлы по категориям
+        public bool SeparateFilesByCategory { get; set; } = false; 
     }
 }
